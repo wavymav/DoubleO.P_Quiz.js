e265675b41665c0ce31eb93d2a0ab071fb62d842
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-25 12:22:35 -0400

    added Quiz & Question constructors

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+	var questionElement = document.getElementById('question'),
+			questionNumberElement = document.getElementById('question-number'),
+			questionCountElement = document.getElementById('question-count'),
+			quizSelectionElement = document.getElementById('quiz-selection');
+
+
+	function Question(question, answer, selections) {
+		this.question = question;
+		this.answer = answer;
+		this.selections = selections;
+	}
+
 	function Quiz(questions) {
 		this.questions = questions;
+		this.score = 0;
+		this.currentCount = 0;
 	}
 });
